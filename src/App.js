3d29b65c753af0238697095eec43b79b81892393
dpak1999 <deepakkumardash313@gmail.com>
2021-12-08 21:04:28 +0530

    setting disabled and active states

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Stock from './Stock';
 
 function App() {
   return (
-    <div className="bg-black min-h-screen">
-      <h1 className="text-white text-2xl text-center font-mono pt-3">
+    <div className="">
+      <h1 className="text-2xl text-center font-mono pt-3">
         Calculate profit or loss on your stocks
       </h1>
       <Stock />
--- src/Stock.js
+++ src/Stock.js
@@ -1,27 +1,71 @@
 /** @format */
 
+import { useState } from 'react';
+
 const Stock = () => {
+  const [state, setState] = useState({
+    buyPrice: '',
+    qty: '',
+    sellPrice: '',
+  });
+  const [disabled, setDisabled] = useState(true);
+
+  const handleChange = (e) => {
+    const val = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: val,
+    });
+    if (state.buyPrice !== '' && state.qty !== '' && state.sellPrice !== '') {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  const handleClick = () => {
+    console.log(state);
+    console.log(disabled);
+  };
+
   return (
-    <div className="text-white text-center">
-      <h1 className="text-white mt-3 font-mono">Enter buying price</h1>
+    <div className=" text-center">
+      <h1 className=" mt-3 font-mono">Enter buying price</h1>
       <input
+        name="buyPrice"
+        value={state.buyPrice}
+        onChange={handleChange}
         type="number"
-        className="bg-transparent text-white border border-purple-500 rounded w-80 p-2 outline-none mt-1"
+        className="bg-transparent  border border-purple-500 rounded w-80 p-2 outline-none mt-1"
       />
 
-      <h1 className="text-white mt-3 font-mono">Enter quantity</h1>
+      <h1 className=" mt-3 font-mono">Enter quantity</h1>
       <input
+        name="qty"
+        value={state.qty}
+        onChange={handleChange}
         type="number"
-        className="bg-transparent text-white border border-purple-500 rounded w-80 p-2 outline-none mt-1"
+        className="bg-transparent  border border-purple-500 rounded w-80 p-2 outline-none mt-1"
       />
 
-      <h1 className="text-white mt-3 font-mono">Enter current price</h1>
+      <h1 className=" mt-3 font-mono">Enter current price</h1>
       <input
+        name="sellPrice"
+        value={state.sellPrice}
+        onChange={handleChange}
         type="number"
-        className="bg-transparent text-white border border-purple-500 rounded w-80 p-2 outline-none mt-1"
+        className="bg-transparent  border border-purple-500 rounded w-80 p-2 outline-none mt-1"
       />
       <br />
-      <button className="border border-purple-500 py-1 px-5 rounded mt-7">
+      <button
+        disabled={disabled}
+        className={`border py-1 px-5 rounded mt-7 ${
+          disabled
+            ? `cursor-not-allowed border-gray-400 text-gray-400`
+            : `border-purple-500 text-purple-500`
+        }`}
+        onClick={handleClick}
+      >
         Check
       </button>
     </div>
